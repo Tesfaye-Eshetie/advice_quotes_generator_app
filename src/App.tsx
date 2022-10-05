80b59002409afcdd80ad417d91ae144d98565023
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-10-04 22:57:44 -0400

    finished the ui and fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = async () => {
+    try {
+      const { data } = await axios("https://api.adviceslip.com/advice");
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <div className="card">
+        <h2 className="heading">{advice}</h2>
+        <button className="button" onClick={fetchAdvice}>
+          GIVE ME ADVICE!
+        </button>
+      </div>
     </div>
   );
 }
